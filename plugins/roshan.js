8061a28f1bec64ae14d69f9c146b572c905f59af
MohammedRoshanT <70361124+MohammedRoshanT@users.noreply.github.com>
2021-11-09 20:24:09 +0400

    Update roshan.js

--- plugins/roshan.js
+++ plugins/roshan.js
@@ -4,7 +4,7 @@ use this git under copyright
 dont change credit
 */
 
-const Roshan= require('../events');
+const Roshan = require('../events');
 const config = require('../config');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
