1a84d6489c8a3215a6b5800d2b7fb6ec93fdc83a
MohammedRoshanT <70361124+MohammedRoshanT@users.noreply.github.com>
2021-11-12 13:22:16 +0400

    Small update

--- Pinky.js
+++ Pinky.js
@@ -1,17 +1,17 @@
-/* codded by afnanplk
+/* By Roshan
 */
 
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
-// PLK Special Functions
+
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 
 module.exports = {
  PLKAFN: process.env.BAD_WORDS === undefined ? false : process.env.BAD_WORDS,
- PHONE: process.env.NUMBER === undefined ? '+919895828468' : process.env.NUMBER,   
- OA_NAME: process.env.DEPLOYER === undefined ? 'Amalser' : process.env.DEPLOYER,    
+ PHONE: process.env.NUMBER === undefined ? '+918848859106' : process.env.NUMBER,   
+ OA_NAME: process.env.DEPLOYER === undefined ? 'RoshanSer' : process.env.DEPLOYER,    
 };
